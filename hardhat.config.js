3d3379bd714652376fbf082ab2758f85bb4d8270
Marticat <157224638+Marticat@users.noreply.github.com>
2025-02-06 12:27:37 +0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.20",  // Версия Solidity
   networks: {
     holesky: {
-      url: "https://ethereum-holesky-rpc.publicnode.com/",  // RPC URL для Holesky
-      accounts: ['0x353a1bc00315b4b6b7191514644d9de4256eaed462602767c87543f19c1cf3f8'],      // Приватный ключ для деплоя
+      url: "",  // RPC URL для Holesky
+      accounts: [''],      // Приватный ключ для деплоя
     },
   },
 };
